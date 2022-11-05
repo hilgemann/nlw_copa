5f15cd05263bef486a29cd515a6006913cd4b511
William Hilgemann <william.hilgemann@gmail.com>
2022-11-04 22:28:54 -0300

    Aula 02.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading'
